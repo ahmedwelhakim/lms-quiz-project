35da33e166dd59f328b3adb2938f91f3e3b68457
Hesham87 <68350777+Hesham87@users.noreply.github.com>
2021-05-21 15:25:43 +0200

    Delete Questions Sample.sql

--- db/Questions Sample.sql
+++
@@ -1,5 +0,0 @@
-insert into questions values('What does HTML stand for','Home tool Markup language','Hyperlinks and Text Markup language','Hyper Text Markup language','none of the above','Home tool Markup language');
-insert into questions values('look at the following selector: $("div"). What does it select?','The first divs element','All div elements','The last div element','none of the above','The first divs element');
-insert into questions values('Where is the correct place to insert a javascript?','the head section','The body section','Both the head and body','none of the above','the head section');
-insert into questions values('How do you create a function in javascript?','function myFunction()','function:myFunction()','function=myFunction()','none of the above','function myFunction()');
-insert into questions values('Which class provides a responsive fixed width container?','container','container-fixed','container-fluid','none of these','container');
