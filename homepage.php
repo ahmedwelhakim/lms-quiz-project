849d62d54e5f52094a426511ddfa902086359ceb
Hesham87 <68350777+Hesham87@users.noreply.github.com>
2021-05-21 15:27:33 +0200

    Update homepage.php

--- homepage.php
+++ homepage.php
@@ -8,7 +8,7 @@ if (!isset($_SESSION['username'])) {
 //connect the server to the database
 include("db/dbConnection.php");
 include("db/dbFunctions.inc.php");
-createQus($conn, 'What does HTML stand for','Home tool Markup language','Hyperlinks and Text Markup language','Hyper Text Markup language','none of the above','Home tool Markup language');
+createQus($conn, 'What does HTML stand for?','Home tool Markup language','Hyperlinks and Text Markup language','Hyper Text Markup language','none of the above','Home tool Markup language');
 createQus($conn, 'look at the following selector: $("div"). What does it select?','The first divs element','All div elements','The last div element','none of the above','The first divs element');
 createQus($conn, 'Where is the correct place to insert a javascript?','the head section','The body section','Both the head and body','none of the above','the head section');
 createQus($conn, 'How do you create a function in javascript?','function myFunction()','function:myFunction()','function=myFunction()','none of the above','function myFunction()');
