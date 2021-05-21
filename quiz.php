09510b9e7ccf4d02ba90078a121b62c6d909bd12
Ahmed Walid <ahmed.w.elhakim@gmail.com>
2021-05-21 17:36:20 +0200

    creating quiz, quiz creation, student, proffessor pages

--- quiz.php
+++ quiz.php
@@ -5,14 +5,8 @@ if (!isset($_SESSION['username'])) {
     header('location:index.php');
 }
 
-//connect the server to the database
 include("db/dbConnection.php");
 include("db/dbFunctions.inc.php");
-createQus($conn, 'What does HTML stand for','Home tool Markup language','Hyperlinks and Text Markup language','Hyper Text Markup language','none of the above',1);
-createQus($conn, 'look at the following selector: $("div"). What does it select?','The first divs element','All div elements','The last div element','none of the above',1);
-createQus($conn, 'Where is the correct place to insert a javascript?','the head section','The body section','Both the head and body','none of the above',1);
-createQus($conn, 'How do you create a function in javascript?','function myFunction()','function:myFunction()','function=myFunction()','none of the above',1);
-createQus($conn, 'Which class provides a responsive fixed width container?','container','container-fixed','container-fluid','none of these',1);
 
 ?>
 <!DOCTYPE html>
@@ -46,7 +40,7 @@ createQus($conn, 'Which class provides a responsive fixed width container?','con
     </div>
     <div class="main-body">
         <div class="quiz">
-            <form action="checked.php" method="post">
+            <form action="process/checked.php" method="post">
                 <?php
                 for ($i = 1; $i < 6; $i++) {
                     $l = 1;
@@ -77,7 +71,7 @@ createQus($conn, 'Which class provides a responsive fixed width container?','con
                             </div>
 
                             <br>
-                            <input type="submit" name="submit" Value="Submit" class="btn btn-success m-auto d-block" /> <br>
+                            <input type="submit" name="submit" Value="Submit" id="btn" class="btn-success m-auto d-block" /> <br>
             </form>
         </div>
     </div>
---
+++ student_home.php
@@ -0,0 +1,55 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['username'])) {
+    header('location:index.php');
+}
+
+//connect the server to the database
+include("db/dbConnection.php");
+include("db/dbFunctions.inc.php");
+createQus($conn, 'What does HTML stand for', 'Home tool Markup language', 'Hyperlinks and Text Markup language', 'Hyper Text Markup language', 'none of the above', 1);
+createQus($conn, 'look at the following selector: $("div"). What does it select?', 'The first divs element', 'All div elements', 'The last div element', 'none of the above', 1);
+createQus($conn, 'Where is the correct place to insert a javascript?', 'the head section', 'The body section', 'Both the head and body', 'none of the above', 1);
+createQus($conn, 'How do you create a function in javascript?', 'function myFunction()', 'function:myFunction()', 'function=myFunction()', 'none of the above', 1);
+createQus($conn, 'Which class provides a responsive fixed width container?', 'container', 'container-fixed', 'container-fluid', 'none of these', 1);
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>homepage</title>
+
+</head>
+
+<body>
+
+    <div class="nav-container">
+        <nav id="nav">
+            <div class="logo">
+                My LMS
+            </div>
+            <div class="nav-list"></div>
+            <div class="nav-login">
+                <ul class="nav-list">
+                    <li>Welcome <?php echo ($_SESSION['username']); ?></li>
+                </ul>
+            </div>
+        </nav>
+
+        </nav>
+    </div>
+    <div class="main-body">
+        <h3> Your quiz </h3>
+        <form action="quiz.php">
+            <button id="btn">Quiz</button>
+        </form>
+    </div>
+</body>
+
+</html>
