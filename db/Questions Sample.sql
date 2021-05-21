2bd89dac6002624b26ab8065bf44a90a1402e67b
Hesham87 <68350777+Hesham87@users.noreply.github.com>
2021-05-21 15:25:04 +0200

    Add files via upload

--- db/Questions Sample.sql
+++ db/Questions Sample.sql
@@ -1,31 +1,5 @@
-insert into questions values(1,'What does HTML stand for',1);
-insert into questions values(2,'look at the following selector: $("div"). What does it select?',5);
-insert into questions values(3,'Where is the correct place to insert a javascript?',9);
-insert into questions values(4,'How do you create a function in javascript?',13);
-insert into questions values(5,'Which class provides a responsive fixed width container?',17);
-
-
-insert into answers values (1,'Home tool Markup language',1);
-insert into answers values (2,'Hyperlinks and Text Markup language',1);
-insert into answers values (3,'Hyper Text Markup language',1);
-insert into answers values (4,'none of the above',1);
-
-insert into answers values (5,'The first divs element',2);
-insert into answers values (6,'All div elements',2);
-insert into answers values (7,'The last div element',2);
-insert into answers values (8,'none of the above',2);
-
-insert into answers values (9,'the head section',3);
-insert into answers values (10,'The body section',3);
-insert into answers values (11,'Both the head and body',3);
-insert into answers values (12,'none of the above',3);
-
-insert into answers values (13,'function myFunction()',4);
-insert into answers values (14,'function:myFunction()',4);
-insert into answers values (15,'function=myFunction()',4);
-insert into answers values (16,'none of the above',4);
-
-insert into answers values (17,'container',5);
-insert into answers values (18,'container-fixed',5);
-insert into answers values (19,'container-fluid',5);
-insert into answers values (20,'none of these',5);
+insert into questions values('What does HTML stand for','Home tool Markup language','Hyperlinks and Text Markup language','Hyper Text Markup language','none of the above','Home tool Markup language');
+insert into questions values('look at the following selector: $("div"). What does it select?','The first divs element','All div elements','The last div element','none of the above','The first divs element');
+insert into questions values('Where is the correct place to insert a javascript?','the head section','The body section','Both the head and body','none of the above','the head section');
+insert into questions values('How do you create a function in javascript?','function myFunction()','function:myFunction()','function=myFunction()','none of the above','function myFunction()');
+insert into questions values('Which class provides a responsive fixed width container?','container','container-fixed','container-fluid','none of these','container');
--- db/dbFunctions.inc.php
+++ db/dbFunctions.inc.php
@@ -21,6 +21,54 @@ function checkAns($conn, $questionID, $ans)
     mysqli_stmt_close($stmt);
 }
 
+function getQuestion($conn, $questionID)
+{
+    $sql = "SELECT * FROM questions WHERE qid = ?;";
+    $stmt = mysqli_stmt_init($conn);
+    if (!mysqli_stmt_prepare($stmt,$sql))
+    {
+        header("location: ../index.php?errpr=stmtfailed");
+        exit();
+    }
+    mysqli_stmt_bind_param($stmt, "s", $questionID);
+    mysqli_stmt_execute($stmt);
+    $result =mysqli_stmt_get_result($stmt);
+
+    if ($row = mysqli_fetch_assoc($result))
+    {
+        return $row;
+    }
+    else
+    {
+        return false;
+    }
+    mysqli_stmt_close($stmt);
+}
+
+function getAns($conn, $questionID)
+{
+    $sql = "SELECT * FROM questions WHERE qid = ?;";
+    $stmt = mysqli_stmt_init($conn);
+    if (!mysqli_stmt_prepare($stmt,$sql))
+    {
+        header("location: ../index.php?errpr=stmtfailed");
+        exit();
+    }
+    mysqli_stmt_bind_param($stmt, "s", $questionID);
+    mysqli_stmt_execute($stmt);
+    $result =mysqli_stmt_get_result($stmt);
+
+    if ($row = mysqli_fetch_assoc($result))
+    {
+        return $row["answer"];
+    }
+    else
+    {
+        return false;
+    }
+    mysqli_stmt_close($stmt);
+}
+
 function createQus($conn, $question, $choice1, $choice2, $choice3, $choice4, $ans)
 {
     $sql = "INSERT INTO questions (question, choice1, choice2, choice3, choice4, answer) values (?,?,?,?,?,?);";
